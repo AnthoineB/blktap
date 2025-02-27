de6a5527fcc28c63d6ec9c33c7e88dd8b167c94a
Anthoine Bourgeois <anthoine.bourgeois@vates.tech>
2025-02-27 17:31:59 +0100

    qcow2: wake up bh only when all the iovec are queued

--- drivers/tapdisk-vbd.c
+++ drivers/tapdisk-vbd.c
@@ -1689,8 +1689,8 @@ tapdisk_vbd_issue_request(td_vbd_t *vbd, td_vbd_request_t *vreq)
 		goto fail;
 	}
 
-	for (i = 0; i < vreq->iovcnt; i++) {
-		struct td_iovec *iov = &vreq->iov[i];
+	for (i = vreq->iovcnt - 1; i >= 0; i--) {
+		struct td_iovec *iov = &vreq->iov[vreq->iovcnt - i - 1];
 
 		treq.sidx           = i;
 		treq.buf            = iov->base;
